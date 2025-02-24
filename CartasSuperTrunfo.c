f2f5ff8a18f919b06725ea25d4865882354ebd75
Guilherme-yy <Guilhermedeoliveiraafonso11@gmail.com>
2025-02-24 20:06:13 +0000

    criei o jogo SuperTrunfo nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,63 @@
 #include <stdio.h>
+int main(){
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+char estado1 [30], estado2[30]; 
+char codigo1[10], codigo2[10];
+char cidade1[30], cidade2[30];
+int populacao1, populacao2;
+int pontos_turisticos1, pontos_turisticos2;
+float PIB1, PIB2;
+float area1, area2;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+// primeira carta 
 
-    return 0;
+  printf("digite o primeiro estado:\n");
+  scanf(" %s", estado1);
+
+  printf("digite o codigo do primeiro estado:\n ");
+  scanf(" %s", codigo1);
+
+  printf("digite o nome da capital do primeiro estado:\n");
+  scanf(" %s", cidade1);
+
+  printf("digite o valor da população com numero inteiro(sem o uso de vírgula e espaço) do primeiro estado:\n ");
+  scanf(" %d", &populacao1);  //porque pedi para não usar vírgula e espaço? porque o uso do scanf() da erro no terminal
+  //pois o certo seria usar o fflush(stdin) ou getchar() caso coleque virgula/espaço o terminal vai pular todas as outras perguntas da primeira carta e começar a da segunda carta
+  
+  
+  printf("digite o valor do PIB do primeiro estado:\n");
+  scanf("%f", &PIB1);
+
+  printf("digite quantos km² tem seu primeiro estado\n ");
+  scanf("%f", &area1);
+
+  printf("digite quantos pontos turisticos existem no primeiro estado:\n");
+  scanf("%d", &pontos_turisticos1);
+
+//segunda carta
+
+  printf("digite o segundo estado:\n");
+  scanf(" %s", estado2);
+
+  printf("digite o codigo do segundo estado:\n ");
+  scanf(" %s", codigo2);
+
+  printf("digite o nome da capital do segundo estado:\n");
+  scanf(" %s", cidade2);
+
+  printf("digite o valor da população com numero inteiro(sem o uso de vírgula e espaço) do segundo estado:\n ");
+  scanf(" %d", &populacao2 );
+  
+  printf("digite o valor do PIB do segundo estado:\n");
+  scanf("%f", &PIB2);
+
+  printf("digite quantos km² tem seu segundo estado:\n ");
+  scanf("%f", &area2);
+
+  printf("digite quantos pontos turisticos existem no segundo estado:\n");
+  scanf("%d", &pontos_turisticos2);
+
+
+ return 0;
 }
+
